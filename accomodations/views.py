2e8339fd12a4e0a1edea1f6b4e93e96abaaa9edc
khushie00 <khushijaiswal489@gmail.com>
2025-06-25 15:15:05 +0530

    crud views created

--- accomodations/views.py
+++ accomodations/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Accommodation
+from .serializers import AccommodationSerializer
+
+class AccommodationListCreateView(generics.ListCreateAPIView):
+    queryset = Accommodation.objects.all()
+    serializer_class = AccommodationSerializer
+    permission_classes = [IsAuthenticated]
+
+class AccommodationRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Accommodation.objects.all()
+    serializer_class = AccommodationSerializer
+    permission_classes = [IsAuthenticated]
