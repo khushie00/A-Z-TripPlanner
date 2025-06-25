7a268d14a061207e2143eb14fff0779a70c61f3f
khushie00 <khushijaiswal489@gmail.com>
2025-06-25 14:07:02 +0530

    models created for destination

--- destinations/models.py
+++ destinations/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)
+    is_popular = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
