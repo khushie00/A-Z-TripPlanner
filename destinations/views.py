d7a5a2b159e88c9ef3f30e2ea66072aca4ea963a
khushie00 <khushijaiswal489@gmail.com>
2025-06-25 14:07:52 +0530

    crud views created for destination

--- destinations/views.py
+++ destinations/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics, filters
+from .models import Destination
+from .serializers import DestinationSerializer
+
+class DestinationListCreateView(generics.ListCreateAPIView):
+    queryset = Destination.objects.all()
+    serializer_class = DestinationSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'country', 'city', 'state']
+
+class DestinationRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Destination.objects.all()
+    serializer_class = DestinationSerializer
