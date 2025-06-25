c26a7bc70dbdea205509a917e31fb5648069c9b5
khushie00 <khushijaiswal489@gmail.com>
2025-06-25 15:14:30 +0530

    accommodation models created

--- accomodations/models.py
+++ accomodations/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from trips.models import Trip
 
-# Create your models here.
+class Accommodation(models.Model):
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='accommodations')
+    hotel_name = models.CharField(max_length=150)
+    address = models.TextField()
+    check_in = models.DateField()
+    check_out = models.DateField()
+    cost = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.hotel_name} for {self.trip.title}"
